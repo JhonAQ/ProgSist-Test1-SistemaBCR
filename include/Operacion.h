a55331542e32e73c516c20b5df8cc1a7686f7e5f
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2025-06-28 11:44:12 -0500

    Add three operating specializations

--- include/Operacion.h
+++ include/Operacion.h
@@ -1,37 +1,47 @@
 #pragma once
+
 #include <string>
 #include <vector>
-#include "Patrimonio.h"
+#include <memory>
+#include "enums.h"
+#include "Activo.h"
 
-using namespace std;
-
-class EntidadBancaria;
-class Transportadora;
-class Plaza;
+// Forward declaration
+class Boveda;
 
 class Operacion {
-private:
-  string fecha;
-  string tipo;
-  EntidadBancaria* origen;
-  EntidadBancaria* destino;
-  Transportadora* transportadora;
-  Plaza* plaza;
-  
-  vector<Patrimonio*> patrimonios;
-
 public:
-  Operacion(string fecha, string tipo,
-            EntidadBancaria* origen, EntidadBancaria* destino,
-            Transportadora* transportadora, Plaza* plaza);
-  
-  ~Operacion();
-  
-  void agregarPatrimonio(Patrimonio* patrimonio);
-  double calcularMontoTotal() const;
-  void aplicar();
-  
-  string getFecha() const;
-  const vector<Patrimonio*>& getPatrimonios() const;
-  Plaza* getPlaza() const;
+  Operacion(const std::string& id, Boveda* origen, Boveda* destino);
+  virtual ~Operacion() = default;
+
+  void agregarActivo(std::unique_ptr<Activo> activo);
+  virtual void ejecutar() = 0;
+
+  const std::string& obtenerId() const { return idOperacion; }
+  EstadoOperacion obtenerEstado() const { return estado; }
+
+protected:
+  std::string idOperacion;
+  EstadoOperacion estado;
+  Boveda* origen;
+  Boveda* destino;
+  std::vector<std::unique_ptr<Activo>> activosTransferidos;
+};
+
+class TraspasoInterno : public Operacion {
+public:
+  using Operacion::Operacion;
+  void ejecutar() override;
+};
+
+class TransferenciaInterbancaria : public Operacion {
+public:
+  using Operacion::Operacion;
+  void ejecutar() override;
+};
+
+class OperacionBCRP : public Operacion {
+public:
+  using Operacion::Operacion;
+  void ejecutar() override;
 };
